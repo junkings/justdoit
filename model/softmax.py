61c87258aaa445b3718f0653e287c4a5736edb63
junkings <876142347@qq.com>
2017-10-30 08:56:40 +0800

    tijiao

--- model/softmax.py
+++ model/softmax.py
@@ -4,6 +4,7 @@ __author__="jinjun"
 import config
 from algorithm import dataprocessiong as dp
 import numpy as np
+import gc
 
 datatrain, data_test = dp.readdata()
 # print(datatrain)
@@ -20,17 +21,35 @@ for cont in datashop:
         shop[shop_id] = {}
         shop[shop_id]["latitude"] = datashop[cont]['latitude']
         shop[shop_id]["longitude"] = datashop[cont]["longitude"]
+        shop[shop_id]['mall_id'] = datashop[cont]['mall_id']
 
 # 特征处理
+# 特征选择 时间
 falsenum = 0
 num = 0
+wifi = {}
 for cont in datatrain:
-    datatrain[cont]['time_stamp'] = datatrain[cont]['time_stamp'].strip(' ')[1]
+    # print(datatrain[cont])
+    # input()
+    datatrain[cont]['time_stamp'] = datatrain[cont]['time_stamp'].split(' ')[1]
+
+    listwifi = datatrain[cont]['wifi_infos'].strip(';')
+    wificont = 0
+    for info in listwifi:
+        each = info.split('|')[0]
+        if each not in wifi:
+            wifi[each] = {}
+            wifi[each] = wificont
+            wificont += 1
+
+
+
     datatrain[cont]["dis"] = {}
     mindis = 10000.0
     r_shop = ""
     for shop_id in shop:
-
+        if datatrain[cont]['maill_id'] != shop[shop_id]['mall_id']:
+            continue
         tmp_latitude = float(shop[shop_id]['latitude']) - float(datatrain[cont]['latitude'])
         tmp_longitude = float(shop[shop_id]['longitude']) - float(datatrain[cont]['longitude'])
         dis = np.sqrt(tmp_latitude*tmp_latitude+tmp_longitude*tmp_longitude)
@@ -43,7 +62,7 @@ for cont in datatrain:
         falsenum += 1
     num += 1
 
-print(falsenum /num)
+print(float(falsenum) /num)
 
 
 
